d47e87193cd34cf8f7e481f53183c24b6b27d1cd
Lisa Leung <ljleung2@illinois.edu>
2022-12-11 23:17:37 +0000

    BC.cpp & readme instructions

---
+++ build/CMakeFiles/PageRank.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PageRank.dir/entry/PageRank.cpp.o"
+  "CMakeFiles/PageRank.dir/entry/PageRank.cpp.o.d"
+  "PageRank"
+  "PageRank.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PageRank.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
